9e5186b7478e6b276ac94d4f4c60bf8488a33ea0
Nayan Kumar <naayaankumar@gmail.com>
2025-02-02 20:21:55 +0530

    Compose testing done

--- src/components/InputCompnent.tsx
+++ src/components/InputCompnent.tsx
@@ -10,13 +10,13 @@ export default function InputComponent({
   return (
     <div className="w-full max-w-full min-w-[200px] ">
       <label className="text-white font-medium mb-1 w-full">
-        {type == 'Date' ? 'Date' : 'Text Input'}
+        {type === 'Date' ? 'Date' : 'Text Input'}
       </label>
       <input
-        onChange={type == 'text' ? titleHandler : DateHandler}
+        onChange={type === 'text' ? titleHandler : DateHandler}
         className="w-full bg-transparent placeholder:text-white text-white text-sm border border-gray-300 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-        placeholder="Type here..."
         type={type}
+        placeholder={type === 'date' ? 'dd/mm/yyyy' : 'Type Here..'}
       />
     </div>
   );
